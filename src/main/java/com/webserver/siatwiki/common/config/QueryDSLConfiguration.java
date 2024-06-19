fe5d01184abe7d5c90840cd885dfa2baf5438b79
hg_yellow <jang010505@gmail.com>
2024-06-19 16:37:09 +0900

    Init project

--- src/main/java/com/webserver/siatwiki/common/config/QueryDSLConfiguration.java
+++ src/main/java/com/webserver/siatwiki/common/config/QueryDSLConfiguration.java
@@ -1,7 +1,7 @@
 package com.webserver.siatwiki.common.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.PersistenceContext;;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
