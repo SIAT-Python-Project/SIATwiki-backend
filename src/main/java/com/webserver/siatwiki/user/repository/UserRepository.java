9782cb5990d35178ba2b2bc8568a2e5393d16af8
Changchang <bbmini96@hanmail.net>
2024-06-21 09:03:31 +0900

    #11 User insert, login, finAll

--- src/main/java/com/webserver/siatwiki/user/repository/UserRepository.java
+++ src/main/java/com/webserver/siatwiki/user/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.webserver.siatwiki.user.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.webserver.siatwiki.user.entity.User;
@@ -13,5 +12,4 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
    public User findByEmail(String email);
 
-   public User findByEmailAndPassword(String email, String password);
 }
