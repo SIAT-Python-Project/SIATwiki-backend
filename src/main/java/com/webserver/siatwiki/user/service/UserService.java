921b442a5cb6736481d4749d37d98eb774630a03
Changchang <bbmini96@hanmail.net>
2024-06-21 09:11:58 +0900

    #11 서버 리빌드

--- src/main/java/com/webserver/siatwiki/user/service/UserService.java
+++ src/main/java/com/webserver/siatwiki/user/service/UserService.java
@@ -6,8 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.webserver.siatwiki.person.dto.PersonDTO;
-import com.webserver.siatwiki.person.entity.Person;
+
 import com.webserver.siatwiki.user.dto.UserDTO.UserRequestDTO;
 import com.webserver.siatwiki.user.entity.User;
 import com.webserver.siatwiki.user.repository.UserRepository;
